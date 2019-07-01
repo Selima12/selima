9eed8d442c986ba7de310652b30b4a478dd1e06e
Selima12 <52163772+Selima12@users.noreply.github.com>
2019-07-01 17:59:49 +0200

    Add files via upload

---
+++ GoMyCode1/Nouveau dossier/js/main.js
@@ -0,0 +1,15 @@
+function bold(){
+    document.getElementById("mytext").style.fontWeight="bold";
+}
+function itallic(){
+    document.getElementById("mytext").style.fontStyle="italic";
+}
+function underline1(){
+    document.getElementById("mytext").style.textDecoration="underline";
+}
+function size1(){
+    document.getElementById("mytext").style.fontSize = document.getElementById("one").value;
+}
+function text1(){
+    document.getElementById("mytext").style.fontFamily= document.getElementById("two").value;
+}
